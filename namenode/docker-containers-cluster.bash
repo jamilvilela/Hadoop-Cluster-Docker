2498a80f4e9d31052836ff79bf16c8591e1d41ad
Jamil Vilela <jamilvilela@gmail.com>
2023-01-22 22:23:42 +0000

    docker file issue fixed

--- namenode/docker-containers-cluster.bash
+++
@@ -1,8 +0,0 @@
-# Image building
-docker build . -t hadoop-namenode:data-lake-jamil
-
-# Network creation for the hadoop cluster
-docker network create -d bridge data-lake-network
-
-# docker run the image inside a container 
-docker run -it -d --net data-lake-network --hostname hdpmaster -p 9870:9870 -p 50030:50030 -p 8020:8020 --name hadoop-namenode hadoop-namenode:data-lake-jamil
---
+++ namenode/docker-namenode-start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# It should be run into the container
+
+# Restart the service ssh
+sudo service ssh restart
+
+# Adjust the privileges
+sudo chown -R hduser:hduser /home/hduser/jdk
+sudo chown -R hduser:hduser /home/hduser/hadoop
+
+# Format the NameNode (just once)
+hdfs namenode -format
+
+# Initialize the namenode
+hdfs --daemon start namenode
