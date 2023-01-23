d3773419e6c92bbd6cfb6e7555118681c0b62068
Jamil Vilela <jamilvilela@gmail.com>
2023-01-23 00:05:31 +0000

    docker run command fixed

--- namenode/docker-containers-cluster.sh
+++ namenode/docker-containers-cluster.sh
@@ -2,14 +2,7 @@
 docker build . -t hadoop-namenode:data-lake-jamil
 
 # Network creation for the hadoop cluster
-docker network create -d bridge data-lake-network
+docker network create -d bridge data_lake_network
 
 # docker run the image inside a container 
-docker run -it -d \
---net data-lake-network \
--v /tmp/hadoop:/data \
---mount source=hadoop_volume,target=/data,type=bind \
---hostname hdpmaster \
--p 9870:9870 -p 50030:50030 -p 8020:8020 \
---name hadoop-namenode \
-hadoop-namenode:data-lake-jamil
+docker run -it -d --net data_lake_network --hostname hdpmaster -p 9870:9870 -p 50030:50030 -p 8020:8020 --name hadoop-namenode hadoop-namenode:data-lake-jamil
